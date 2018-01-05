5e066241bcb59f1e94c2258d1059f17deb170eaa
Will Christerson <wbchristerson@gmail.com>
2018-01-05 08:22:55 -0500

    Include main page buttons

--- database.py
+++ database.py
@@ -2,7 +2,22 @@ from tkinter import *
 
 root = Tk()
 
-root.title("Simple GUI")
+root.title("Database")
 root.geometry("200x100")
+
+app = Frame(root)
+app.grid()
+
+lbl = Label(app, text = "Welcome!")
+lbl.grid()
+
+
+bttn1 = Button(app, text = "Look Up An Entry")
+bttn1.grid()
+
+bttn2 = Button(app)
+bttn2.grid()
+bttn2.configure(text = "Add An Entry")
+
 
 root.mainloop()
