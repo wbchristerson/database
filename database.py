da346a7e50ef35010ce8146dbbec809be6e617d3
Will Christerson <wbchristerson@gmail.com>
2018-01-08 11:21:49 -0500

    Fix bugs but still leave main page blank

--- database.py
+++ database.py
@@ -6,15 +6,18 @@ class Transition(Tk):
         container = Frame(self)
 
         self.title("Database")
-        self.geometry("200x150")
+        self.geometry("200x200")
         self.frames = {}
 
-        #for F in (Application, WritePage):
-        #    frame = F(self, container)
-        #    self.frames[F] = frame
+        for F in (Application, WritePage):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
 
-        self.frames[Application] = Application(self, container)
-        self.frames[WritePage] = WritePage(self, container)
+        #self.frames[Application] = Application(self, container)
+        #self.frame[Application] = Application(container, self)
+        #self.frames[WritePage] = WritePage(self, container)
+        #self.frame[WritePage] = WritePage(container, self)
 
         self.show_frame(Application)
         
@@ -29,48 +32,58 @@ class Application(Frame):
     """ Main page for database """
     """ Object-oriented design based on Michael Dawson's Python Programming
         For The Absolute Beginner """
-    def __init__(self, master, controller):
-        super(Application, self).__init__(master)
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
         self.grid()
-        self.set_buttons(controller)
-
-    def set_buttons(self, controller):
-        """ Create text introduction """
         lbl = Label(self, text = "Welcome!")
         lbl.grid()
+#    def __init__(self, master, controller):
+#        super(Application, self).__init__(master)
+#       self.grid()
+#        self.set_buttons(controller)
 
-        self.bttn1 = Button(self, text = "Look Up An Entry",
-                            command=lambda: controller.show_frame(WritePage))
-        self.bttn1.grid()
+#    def set_buttons(self, controller):
+#        """ Create text introduction """
+#        lbl = Label(self, text = "Welcome!")
+#        lbl.grid()
 
-        self.bttn2 = Button(self, text = "Browse Entries")
-        self.bttn2.grid()
+#       self.bttn1 = Button(self, text = "Look Up An Entry",
+#                            command=lambda: controller.show_frame(WritePage))
+#        self.bttn1.grid()
 
-        self.bttn3 = Button(self)
-        self.bttn3.grid()
-        self.bttn3.configure(text = "Add An Entry")
+#       self.bttn2 = Button(self, text = "Browse Entries")
+#        self.bttn2.grid()
 
-        self.bttn4 = Button(self)
-        self.bttn4.grid()
-        self.bttn4["text"] = "Edit An Entry"
+#        self.bttn3 = Button(self)
+#        self.bttn3.grid()
+#        self.bttn3.configure(text = "Add An Entry")
+
+#        self.bttn4 = Button(self)
+#        self.bttn4.grid()
+#        self.bttn4["text"] = "Edit An Entry"
 
 
 class WritePage(Frame):
     """ Main page for database """
     """ Object-oriented design based on Michael Dawson's Python Programming
         For The Absolute Beginner """
-    def __init__(self, master, controller):
-        super(WritePage, self).__init__(master)
-        self.grid()
-        self.set_buttons()
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        #self.grid()
+        #lbl = Label(self, text = "Bienvenidos!")
+        #lbl.grid()
+#    def __init__(self, master, controller):
+#        super(WritePage, self).__init__(master)
+#        self.grid()
+#        self.set_buttons()
 
-    def set_buttons(self):
-        """ Create text introduction """
-        lbl = Label(self, text = "Write Page!")
-        lbl.grid()
+#    def set_buttons(self):
+#        """ Create text introduction """
+#        lbl = Label(self, text = "Write Page!")
+#        lbl.grid()
 
-        self.bttn1 = Button(self, text = "Write In An Entry")
-        self.bttn1.grid()
+#        self.bttn1 = Button(self, text = "Write In An Entry")
+#        self.bttn1.grid()
 
 
 # main
