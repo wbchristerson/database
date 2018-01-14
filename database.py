645e64a33696c06982b7d47fc60646eba31fb2db
Will Christerson <wbchristerson@gmail.com>
2018-01-14 10:51:33 -0500

    Space buttons vertically in introductory page

--- database.py
+++ database.py
@@ -13,6 +13,10 @@ class Transition(Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+        # Added Weight
+        #container.grid_columnconfigure(2, weight=1)
+        # Added Weight
+
         self.title("Database")
         self.geometry("300x300")
         self.frames = {}
@@ -21,6 +25,7 @@ class Transition(Tk):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
+            #frame.grid(row=0, column=0)
 
         self.show_frame(Application)
         
@@ -37,8 +42,12 @@ class Application(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         self.grid()
+        
         lbl = Label(self, text = "Welcome!")
-        lbl.grid(row=1,column=3)
+        #lbl.grid(row=1,column=3)
+        #lbl.grid_rowconfigure(0, weight=2)
+        lbl.grid(row=1,column=1)
+        lbl.grid_columnconfigure(1, weight=1)
         self.set_buttons(controller)
 
     def set_buttons(self, controller):
@@ -46,17 +55,22 @@ class Application(Frame):
 
         self.bttn1 = Button(self, text = "Look Up An Entry",
                             command=lambda: controller.show_frame(WritePage))
-        self.bttn1.grid(row=2,column=3)
+        #self.bttn1.grid(row=2,column=3)
+        self.bttn1.grid(row=2,column=1, pady=10)
+        self.bttn1.grid_rowconfigure(2, weight=1)
 
         self.bttn2 = Button(self, text = "Browse Entries")
-        self.bttn2.grid(row=4,column=3)
+        #self.bttn2.grid(row=4,column=3)
+        self.bttn2.grid(row=4,column=1, pady=10)
 
         self.bttn3 = Button(self)
-        self.bttn3.grid(row=6,column=3)
+        #self.bttn3.grid(row=6,column=3)
+        self.bttn3.grid(row=6,column=1, pady=10)
         self.bttn3.configure(text = "Add An Entry")
 
         self.bttn4 = Button(self)
-        self.bttn4.grid(row=8,column=1)
+        #self.bttn4.grid(row=8,column=3)
+        self.bttn4.grid(row=8,column=1, pady=10)
         self.bttn4["text"] = "Edit An Entry"
 
 
